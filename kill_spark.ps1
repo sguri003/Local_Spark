1ee8b9736ee347ed773c65267720d82721a4f398
smguri002 <57817751+smguri002@users.noreply.github.com>
2026-08-04 16:22:35 -0400

    added merge example

---
+++ kill_spark.ps1
@@ -0,0 +1,4 @@
+﻿Get-Process python -ErrorAction SilentlyContinue
+Get-Process java -ErrorAction SilentlyContinue
+#kill all process for Spark server
+Get-Process java -ErrorAction SilentlyContinue | Stop-Process -Force
